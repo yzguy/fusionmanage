b9ceab1a685da539cf584e1267e475ff71ff97cf
Adam Smith <zero1three@gmail.com>
2016-11-13 17:27:46 -0800

    turn fusionmanage into a ruby gem

---
+++ lib/fusionmanage.rb
@@ -0,0 +1,33 @@
+class FusionManage
+  @fusionbase = '/Applications/VMware\ Fusion.app/Contents/Library/vmnet-cli'
+  @stop = @fusionbase + ' --stop'
+  @start = @fusionbase + ' --start'
+  @nat_path = "/Library/Preferences/VMware\ Fusion/vmnet8/nat.conf"
+
+  def self.start_network
+    system @start
+  end
+
+  def self.stop_network
+    system @stop
+  end
+
+  def self.restart_network
+    system @start if system @stop
+  end
+
+  def self.port_forward(action, protocol, ipaddr = '', port)
+    case action
+    when 'add'
+      Forward.new(@nat_path).add(protocol, ipaddr, port)
+    when 'delete'
+      Forward.new(@nat_path).delete(protocol, port)
+    end
+  end
+
+  def self.show_forwards
+    Forward.new(@nat_path).show_forwards
+  end
+end
+
+require 'fusionmanage/forward'
---
+++ lib/fusionmanage/forward.rb
@@ -0,0 +1,42 @@
+require 'inifile'
+
+class FusionManage::Forward
+  attr_reader :inifile
+  def initialize(ini_path)
+    @inifile = load(ini_path)
+  end
+
+  def load(ini_path)
+    IniFile.load(ini_path) || abort('Error: inifile not found')
+  end
+
+  def save
+    inifile.write
+  end
+
+  def add(protocol, ipaddr, port)
+    inifile["incoming#{protocol}"][port] = "#{ipaddr}:#{port}"
+  ensure
+    save
+  end
+
+  def delete(protocol, port)
+    inifile["incoming#{protocol}"].delete(port)
+  ensure
+    save
+  end
+
+  def forwards_by_protocol(protocol)
+    puts "#{protocol.upcase} Forwards"
+    inifile["incoming#{protocol}"].each do |key, val|
+      puts "#{key} => #{val}"
+    end
+    puts
+  end
+
+  def show_forwards
+    ['tcp', 'udp'].each do |protocol|
+      forwards_by_protocol(protocol)
+    end
+  end
+end
